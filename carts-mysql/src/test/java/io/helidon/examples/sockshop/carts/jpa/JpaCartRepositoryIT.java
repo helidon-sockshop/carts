9002cb631e4bbf25b5a9c36d575a2493b8751876
Aleksandar Seovic <aleks@seovic.com>
2020-01-31 08:27:30 -0500

    Cleanup

--- carts-mysql/src/test/java/io/helidon/examples/sockshop/carts/jpa/JpaCartRepositoryIT.java
+++ carts-mysql/src/test/java/io/helidon/examples/sockshop/carts/jpa/JpaCartRepositoryIT.java
@@ -1,19 +1,22 @@
 package io.helidon.examples.sockshop.carts.jpa;
 
-import javax.enterprise.inject.se.SeContainer;
-import javax.enterprise.inject.se.SeContainerInitializer;
-
 import io.helidon.examples.sockshop.carts.CartRepository;
 import io.helidon.examples.sockshop.carts.CartRepositoryTest;
+import io.helidon.microprofile.server.Server;
 
 /**
  * Integration tests for {@link io.helidon.examples.sockshop.carts.jpa.JpaCartRepository}.
  */
 public class JpaCartRepositoryIT extends CartRepositoryTest {
 
+    /**
+     * Starting server on ephemeral port in order to be able to get the
+     * fully configured repository from the CDI container.
+     */
+    private static final Server SERVER = Server.builder().port(0).build().start();
+
     @Override
     protected CartRepository getCartRepository() {
-        SeContainer cdi = SeContainerInitializer.newInstance().initialize();
-        return cdi.select(CartRepository.class).get();
+        return SERVER.cdiContainer().select(CartRepository.class).get();
     }
 }
