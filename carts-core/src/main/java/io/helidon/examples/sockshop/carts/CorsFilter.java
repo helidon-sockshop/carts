8d587e0bbe022f6992300634e45d035589cc707f
Aleksandar Seovic <aleks@seovic.com>
2020-02-25 19:38:09 -0500

    Added simple CORS filter implementation in order for Swagger UI to work

---
+++ carts-core/src/main/java/io/helidon/examples/sockshop/carts/CorsFilter.java
@@ -0,0 +1,28 @@
+package io.helidon.examples.sockshop.carts;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.ConstrainedTo;
+import javax.ws.rs.RuntimeType;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * Simple CORS filter.
+ */
+@ApplicationScoped
+@Provider
+@ConstrainedTo(RuntimeType.SERVER)
+public class CorsFilter
+        implements ContainerResponseFilter {
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
+        MultivaluedMap<String, Object> headers = responseContext.getHeaders();
+
+        headers.add("Access-Control-Allow-Origin", "*");
+        headers.add("Access-Control-Allow-Methods", "*");
+        headers.add("Access-Control-Allow-Headers", "Content-Type, *");
+    }
+}
